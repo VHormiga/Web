466a24b3fe295268429a0eded9e234d03c733e1f
Hormiga vincent <hormigavincent@gmail.com>
2024-03-23 22:04:55 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./app.scss"
 
-function App() {
+import { BrowserRouter as Router, Route, matchRoutes, Routes } from 'react-router-dom'
+import ProtedtedRoute from "./utils/ProtectedRoute";
+import { AuthProvider } from './context/AuthContext';
+import {Dashboard, Home, Login, Register} from "./pages"
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <div className='container'>
+            <Router>
+              <AuthProvider>
+                <Routes>
+                  <Route 
+                  path="/dashboard"
+                  element={
+                    <ProtedtedRoute>
+                      <Dashboard />
+                    </ProtedtedRoute>
+                  }/>
+                  <Route 
+                  path="/login"
+                  element={<Login />}
+                  />
+                  <Route 
+                  path="/register"
+                  element={<Register />}
+                  />
+                  <Route 
+                  path="/" exact
+                  element={<Home />}
+                  />
+                </Routes>
+              </AuthProvider>
+            </Router>
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
